57928b635dcfdfe58227ca21dc14e02549b02848
bovealexandre <alexandre.l.bove@gmail.com>
2019-03-14 16:37:57 +0100

    middleware

--- app/app/src/Middlewares/AuthMiddleware.php
+++ app/app/src/Middlewares/AuthMiddleware.php
@@ -14,14 +14,15 @@ class AuthMiddleware {
 
   public function __invoke(Request $request, Response $response, $next) {
     $id=$args['id'];
+    var_dump($id);
     $artid= $this->container->get('db')->prepare('SELECT writer_id FROM articles WHERE id=:id');
     $artid->bindValue('id', $id);
     $artid->execute();
     $writerid = $artid->fetch(\PDO::FETCH_ASSOC);
     if (!isset($_SESSION['permission']) || empty($_SESSION['permission'])) {
-      return $response->withRedirect($this->container->router->pathFor('connexion'));
+      //return $response->withRedirect($this->container->router->pathFor('connexion'));
     }elseif($_SESSION['permission']=== 1 || $_SESSION['permission']=== 2 || $_SESSION['id'] !== $id || $_SESSION['id'] !== $writerid  ){
-      return $response->withRedirect($this->container->router->pathFor('dashboard'));
+      //return $response->withRedirect($this->container->router->pathFor('dashboard'));
     }
     $response = $next($request, $response);
     return $response;
