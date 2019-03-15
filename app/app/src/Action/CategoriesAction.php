f66a45a7c0fbad1fc68cc3c341ca1c99893941cf
bovealexandre <alexandre.l.bove@gmail.com>
2019-03-15 15:50:19 +0100

    trying wysiwyg editor

--- app/app/src/Action/CategoriesAction.php
+++ app/app/src/Action/CategoriesAction.php
@@ -31,7 +31,6 @@ final class CategoriesAction
             $articles = $this->db->prepare('SELECT  category.*, articles.*, users.pseudo FROM category INNER JOIN articles ON category.article_id=articles.id INNER JOIN users ON articles.writer_id = users.ID WHERE category.categories= :id');
             $articles->bindValue('id',$args['id']);
             $articles->execute();
-            $articles->fetchAll();
        
             $args['categories']=$categories;
         $args['articles']=$articles;
